c7780dce50c9485605eaafcefaacb0efc34063a5
Dave                                      Lee <dalee@twilio.com>
2023-06-07 16:51:39 -0700

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNDg4OA/spectral.js?branch_slug=main&token=6540e845-0edc-47e6-bdfd-3c3ba857b592";
+export default { extends: ruleset };
